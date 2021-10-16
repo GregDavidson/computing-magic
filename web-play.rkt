02ca632b41fa5faab814e1c4883e732640531c69
GregDavidson <greg.davidson+git@gmail.com>
2021-10-16 10:28:29 -0700

    nicer

--- web-play.rkt
+++ web-play.rkt
@@ -1,22 +1,34 @@
 #lang racket
-(require xml) ; for xexpr->string
+(require xml) ; for xexpr functions
 (require web-server/servlet)
 (require web-server/servlet-env)
-(require rackunit)
+(require rackunit) ; for unit testing
 
 (struct post (title body))
+; creates four functional procedures:
+; (: post (-> string? string? post?)) ; constructor
+; (: post? (or/c (-> post? #true) (-> (not/c post? #false)))
+; (: post-title (-> post? string?)) ; selector: returns the title field
+; (: post-body (-> post? string?))  ; selector: returns the body field
+
+(define test-post (post "First Post!" "Hey, this is my first post!"))
+(define test-post-as-xml-string '(div ((class "post")) "First post!" (p "This is a first post.")))
+(display (xexpr->string test-post-as-xml-string))
 
 (define BLOG
   (list
-   (post "First Post!" "Hey, this is my first post!")
+   test-post
    (post "No bills!" "Just car change!") ) )
 
 (define (render-post apost)
   "What goes here?" )
 
-#; (check-equal
-    (render-post (post "First post!" "This is a first post."))
-    '(div ((class "post")) "First post!" (p "This is a first post."))
-    "render-post first post" )
+#; (check-equal? (render-post test-post) test-post-as-xml-string "render-post test-post")
 
-(display (xexpr->string '(div ((class "post")) "First post!" (p "This is a first post."))))
+(define (nice-xexpr-string an-xexpr [indent #t])
+  (display-xml/content (xexpr->xml an-xexpr) #:indentation (if indent 'classic 'none)) )
+
+(define (display-xexpr an-xexpr [indent #t])
+  (display (nice-xexpr-string an-xexpr indent)) )
+
+; (display-xexpr test-post-as-xml-string)
