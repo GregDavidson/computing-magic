0f54cadfd0732c277f31cb8bd753f8d9d3bb9176
GregDavidson <greg.davidson+git@gmail.com>
2022-03-13 11:16:19 -0700

    Better for activities

--- Prolog/Activities/append.pl
+++ Prolog/Activities/append.pl
@@ -5,3 +5,33 @@
 % my_append(first-part-of-the-list, rest-of-the-list, the-whole-list)
 my_append([], L, L).
 my_append([H|L1], L2, [H|L3]) :- my_append(L1, L2, L3).
+
+%% Let's append two lists:
+% ?- append([black, white], [red, green, blue], L).
+
+%% Let's find the right suffix:
+% ?- append([black, white], L, [black, white, red, green, blue]).
+
+%% Let's find the right prefix:
+% ?- append(L, [red, green, blue], [black, white, red, green, blue]).
+
+%% Let's split a list in all possible ways:
+%% - between terms, ";" means "or"
+%% - the ";" was typed interactively to ask for another result
+% ?- append(L1, L2, [red, green, blue]).
+%% You'll want to enter ; to get the next solution
+% L2 = [] ;
+
+%% Do these lists satisfy the append relationship?
+% ?- append([black, white], [red, green, blue], [black, white, red, green, blue]).
+
+%% Do these lists satisfy the append relationship?
+% ?- append([black, white], [red, green, blue], [white green]).
+
+%% We can combine terms with "," meaning "and"
+% ?- append(L1, L2, [red, green, blue]), length(L2, 1).
+
+%% and order doesn't matter:
+%   my_append([First|Rest], L2, [First|L3]) :- append(Rest, L2, L3).
+%   my_append([], L, L).
+%   ?- length(L1, 2), my_append(L1, L2, [a, b, c, d, e]).
