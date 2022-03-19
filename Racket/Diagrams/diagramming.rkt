f5eedd6e4e7f3b883ac1b190c415428c7b5ccd71
GregDavidson <greg.davidson+git@gmail.com>
2022-03-19 14:55:07 -0700

    consistently using stream abstraction

--- Racket/Diagrams/diagramming.rkt
+++ Racket/Diagrams/diagramming.rkt
@@ -5,9 +5,34 @@
 
 ; Shall we use lists to structure our diagrams?
 ; Shall we use a purely functional approach?
+; Let's use Streams:
+; - Lists are automatically accepted as Streams
+; - Arrays, Strings, Sequences & more can be wrapped as Streams
+
 ; Shall we use typed/racket?
-; Or shall we be more eclectic?
-; Let's do everything 2 or 3 ways and learn!
+; Maybe eventuallly, but not yet!
+
+;; ** Labels for Objects
+
+; We want labels to be able to label
+; - a complete object
+; - a field of a structure
+; - an element of a vector
+
+(define (label-pic labels)
+  (cond [(pict? labels) labels]
+        [(char? labels) (label-pic (string labels))]
+        [(string? labels) (text labels)]
+        [(number? labels) (label-pic (~a labels))]
+        [(stream? labels) (label-pic (stream-first labels))]
+        [t #f] ) )
+
+(define (label-next labels)
+  (cond [(number? labels) (+ 1 labels)]
+        [(not (stream? labels)) labels]
+        [t (sequence-tail labels 1)] ) )
+
+;; ** Composing Objects
 
 ; Wrap a pic in a box
 ; Either sized just right or of the specified sizes
@@ -20,7 +45,7 @@
           [w (and-max width (pict-width p))]
           [h (and-max height (pict-height p))]
           [box (cc-superimpose p (rectangle (+ extra-width w) h))] )
-    (if label (vl-append box (text label)) box) ) )
+    (if label (vl-append box (label-pic label)) box) ) )
 ; Mystery: When the vl-append arguments are reversed, the top line
 ; of the boxes does not show, even in tests where there should be no label!
 
@@ -29,79 +54,52 @@
 
 (test-boxed-pic)
 
-; Append a list of pictures into a new picture
-(define (append-list pics #:append [f hc-append] #:skosh [extra-width 0])
-  (apply f (cons extra-width pics)) )
+; Append a Stream of Pictures into a new composite Picture
+(define (append-pics pics #:append [f hc-append] #:skosh [extra-width 0])
+  (let ( [args (stream->list pics)] )
+    (apply f (if extra-width (cons extra-width args) args)) ) )
 
-; Append a sequence of pictures into a new picture
-(define (append-seq pics #:append [f hc-append] #:skosh [extra-width 0])
-  (append-list (sequence->list pics) #:append f #:skosh extra-width) )
+;; ** Calculating Sizes
 
-; Find the maximum width and height of a sequence of pictures
-(define (max-pic-seq-width-height pic-seq #:width [width 0] #:height [height 0])
-  (sequence-for-each
-   (λ (p) (let ( [w (pict-width p)] [h (pict-height p)] )
-            (when (> w width) (set! width w))
-            (when (> h height) (set! height h)) ) )
-   pic-seq )
-  (values width height) )
-
-(define (test-max-pic-seq-width-height [pic-seq (in-list (list (circle 10)))] #:width [width 0] #:height [height 0])
-  (let-values ( [(w h) (max-pic-seq-width-height pic-seq)] )
-    (printf "width ~a height ~a\n" w h) ) )
-
-(test-max-pic-seq-width-height)
-
-; Find the maximum width and height of a list of pictures
-(define (max-pic-list-width-height pics #:width [width 0] #:height [height 0])
-  (define (max i j) (if (> i j) i j))
-  (if (null? pics)
+(define (max1 x1 x2) (if (> x1 x2) x1 x2) )
+(define (max2 x1 x2 y1 y2) (values (max1 x1 x2) (max y1 y2)) )
+                          
+; Find the maximum width and height of a stream of pictures
+(define (max-pics-width-height pics #:width [width 0] #:height [height 0])
+  (if (stream-empty? pics)
       (values width height)
-      (max-pic-list-width-height
-       (cdr pics)
-       #:width (max width (pict-width (car pics)))
-       #:height (max height (pict-height (car pics))) ) ) )
+      (let ( [first (stream-first pics)] [rest (stream-rest pics)] )
+        (max-pics-width-height
+         rest
+         #:width (max1 width (pict-width first))
+         #:height (max1 height (pict-height first)) ) ) ) )
 
-(define (test-max-pic-list-width-height [pics (list (circle 10))] #:width [width 0] #:height [height 0])
-  (let-values ( [(w h) (max-pic-list-width-height pics)] )
+(define (test-max-pics-width-height [pics (list (circle 10))] #:width [width 0] #:height [height 0])
+  (let-values ( [(w h) (max-pics-width-height pics)] )
     (printf "width ~a height ~a\n" w h) ) )
 
-(test-max-pic-list-width-height)
+(test-max-pics-width-height)
 
-; Convert a sequence to a list
+;; ** Transforming Sequences of Objects
+
+; Recursively rebuild a stream
 ; Possibly transforming through a mapping function
-; which might possibly want an index value
-; Possibly appending to an existing list
-(define (seq-to-list seq #:map [f #f] #:tail [tail '()] #:index [index #f])
-  (stream-to-list (sequence->stream seq) #:map f #:tail tail #:index index) )
-
-(define (stream-to-list ss #:map [f #f] #:tail [tail '()] #:index [index #f])
+;   which function might possibly want an index or label
+; Possibly appending to an existing stream
+(define (stream-rebuild ss #:map [f #f] #:tail [tail (stream)] #:index [index #f])
   (if (stream-empty? ss)
       tail
       (let ( [first (stream-first ss)] [rest (stream-rest ss)] )
-        (cons
+        (stream-cons
          (if f (if index (f first index) (f first)) first)
-         (stream-to-list rest #:map f #:tail tail #:index (and index (+ 1 index))) ) ) ) )
+         (stream-rebuild rest #:map f #:tail tail #:index (label-next index)) ) ) ) )
 
-; Explode a string into a sequence
-; Possibly transforming through a mapping function
-; Possibly appending to an existing sequence
-(define (seq-explode-string s #:map [f #f] #:tail [tail #f])
-  (let* ( [chars (in-string s)]
-          [mapped-chars (if f (sequence-map f chars) chars)]
-          [joined-chars (if tail (sequence-append mapped-chars tail) mapped-chars) ] )
-          joined-chars ) )
-(append-seq (seq-explode-string
-             "Hello!"
-             #:map (λ (c) (boxed-pic (text (string c)) #:skosh 3))
-             #:tail (in-list (list (boxed-pic (text "\\0") #:skosh 3))) ))
-
-; Explode a string into a list
+; Explode a string into a Stream
 ; Possibly transforming through a mapping function
 ; Possibly appending to an existing list
-(define (list-explode-string s #:map [f #f] #:tail [tail #f])
-  (seq-to-list (in-string s) #:map f #:tail tail) )
-(append-list (list-explode-string "Hello!"
+(define (explode-string s #:map [f #f] #:tail [tail #f])
+  (stream-rebuild (sequence->stream (in-string s)) #:map f #:tail tail) )
+(append-pics (explode-string "Hello!"
                                   #:map (λ (c) (boxed-pic (text (string c)) #:skosh 3))
                                   #:tail (list (boxed-pic (text "\\0") #:skosh 3)) ))
 
@@ -110,17 +108,16 @@
 ; - the max of the sizes of their constituents
 ; - plus a skosh around the width inside the boxes
 (define (boxed-chars-list str #:null [add-null #f] #:index [index #f])
-  (let* ( [pic-seq (seq-explode-string str #:map (λ (c) (text (string c))))]
-          [tail (if add-null (in-list (list (text "\\0"))) #f)]
-          [elements (if tail (sequence-append pic-seq tail) pic-seq)] )
-    (let-values ( [(w h) (max-pic-seq-width-height elements)] )
+  (let* ( [tail (and add-null (stream (text "\\0")))]
+          [elements (explode-string str #:map (λ (c) (text (string c))) #:tail tail)] )
+    (let-values ( [(w h) (max-pics-width-height elements)] )
       (let ( [f (if index
-                    (λ (p i) (boxed-pic p #:width w #:height h #:skosh 2 #:label (~a i)))
+                    (λ (p i) (boxed-pic p #:width w #:height h #:skosh 2 #:label i))
                     (λ (p) (boxed-pic p #:width w #:height h #:skosh 2)) )] )
-        (seq-to-list elements #:map f #:index index) ) ) ) )
+        (stream-rebuild elements #:map f #:index index) ) ) ) )
 
 (define (string-diagram s #:null [add-null #f] #:index [index #f])
-  (append-list (boxed-chars-list s #:null add-null #:index index)) )
+  (append-pics (boxed-chars-list s #:null add-null #:index index)) )
 
 (string-diagram "Hello!" #:null #t)
 (string-diagram "Hello!" #:null #t #:index 0)
