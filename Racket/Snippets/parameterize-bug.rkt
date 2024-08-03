0c7fc997ee3ce2226b83c4a5009ac7f51de91586
GregDavidson <greg.davidson+git@gmail.com>
2024-08-03 14:10:17 -0700

    my-parameter works around make-parameter guard issues

--- Racket/Snippets/parameterize-bug.rkt
+++ Racket/Snippets/parameterize-bug.rkt
@@ -1,12 +1,23 @@
 #lang racket/base
 
-(define *host* (make-parameter "ngender.net" string? 'host))
+;; fix the problems with the make-parameter guard
+;; - ensure it is applied to the initial value
+;; - ensure it returns the value it has checked
+(define (my-parameter value guard name [check-name #f])
+  (define (string x) (format "~a" x))
+  (define (check v)
+    (unless (guard v)
+      (raise-argument-error (string name)
+                            (string (or check-name (object-name guard) 'check))
+                            v ) )
+    v )
+  (make-parameter (check value) check name) )
+
+(define *host* (my-parameter "ngender.net" string? 'host))
+
+(eprintf "[0] host ~a\n" (*host*))
 
 (parameterize ( [*host* "hello"] )
-  (define h (*host*))
-  (eprintf "h ~a\n" h)
-  (eprintf "host ~a\n" (*host*)) )
-
-;; prints: h #t
-;; prints: host #t
-;; racket version: 8.13 [cs]
+  (eprintf "[1] host ~a\n" (*host*))
+  (*host* "hi")
+  (eprintf "[2] host ~a\n" (*host*)) )
