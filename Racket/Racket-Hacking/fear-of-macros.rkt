8b565108f465f0335f3a7cff5a35dcb1d0488e33
GregDavidson <greg.davidson+git@gmail.com>
2025-05-27 18:52:59 -0700

    added show-yourself macro

--- Racket/Racket-Hacking/fear-of-macros.rkt
+++ Racket/Racket-Hacking/fear-of-macros.rkt
@@ -2,13 +2,33 @@
 
 ;; https://www.greghendershott.com/fear-of-macros/Transform_.html
 
+;; One source says this is the default.
+;; Another says that it's only the default in #lang/racket
 (require (for-syntax racket/base))
+
+(define-syntax (show-yourself stx)
+  (printf "show-yourself syntax: ~a\n" stx)
+  (printf "show-yourself syntax->datum: ~a\n" (syntax->datum stx))
+  (datum->syntax stx (list 'quote (list 'Got (syntax->datum stx)))) )
+
+(show-yourself to the world)
 
 (define-syntax-rule (show exp)
   (printf "~a = ~a\n" 'exp exp) )
 
+(newline)
+(expand-once #'(show (+ 2 2)))
 (show #'(+ 2 2))
 (show (+ 2 2))
+
+(define-syntax foo1
+  (λ (stx) (syntax "I am foo1!")) )
+
+(define-syntax (foo2 stx)
+  (syntax "I am foo1!") )
+
+(newline)
+(show #'(foo))
 
 (define-syntax (show-me stx)
        (println  stx)
