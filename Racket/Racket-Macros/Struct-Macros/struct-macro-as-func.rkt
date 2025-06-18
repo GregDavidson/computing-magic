eb54648f075ed8fe6cf259023d46ca07b409bf65
GregDavidson <greg.davidson+git@gmail.com>
2025-06-17 22:09:45 -0700

    Unnested parse functions for ease of testing and understanding.

--- Racket/Racket-Macros/Struct-Macros/struct-macro-as-func.rkt
+++ Racket/Racket-Macros/Struct-Macros/struct-macro-as-func.rkt
@@ -3,10 +3,21 @@
 ;; * (struct/macro ...) development
 
 ;; We're going to develop our eventual macros
-;; i.e. syntax-phase macro transformers
+;; - i.e. syntax-phase macro transformers
 ;; as regular runtime functions for easier development.
 ;; Once they work, we'll shift them into syntax-phase.
 
+;; Some of these functions are currently written to accept either
+;; syntax objects or unwrapped expressions.  Once development is
+;; complete, these will be macros which will only receive syntax
+;; objects and the code allowing otherwise may go away without notice!
+
+;; for tracing
+(define-syntax-rule (show exp)
+  (let ( [exp-val exp] )
+    (printf "~a --> ~a\n" 'exp exp-val)
+    exp-val ) )
+ 
 ;; ** Phase Control Meta-Macros
 
 ;; Allow us to write code which can either happen at
@@ -146,8 +157,12 @@
          (string-append k ":") ]
         [else (error #f "unknown value ~a" k)] ) )
 
-;; wrap exp in syntax if it isn't already
-;; and if stx is, using stx for context.
+;; DEVELOPMENT FEATURE
+;; an alternative to datum->syntax
+;;   where if (1) exp is already syntax
+;;      or if (2) stx isn't syntax
+;;   we return exp as is.
+;; Once development is complete, this may go away without notice!
 (define (struct/macro-stx stx exp)
   (cond [(syntax? exp) exp]
         [(syntax? stx) (datum->syntax stx exp)]
@@ -199,7 +214,8 @@
  ;;   a list of the values p returned,
  ;;   with any that are syntax objects unwrapped
  (define (evalues->list p . args)
-   (printf "p ~a args ~a\n" p args)
+   (show p)
+   (show args)
    (call-with-values (λ () (apply p args)) ; maybe producing multiple values
                      ;; convert all arguments into a list, unwrapping any syntax objects
                      (λ lst (map (λ (stx) (if (syntax? stx) (syntax-e stx) stx)) lst)) ) )
@@ -232,92 +248,141 @@
   
  )
                                      
-;; ** struct/macro
+;; ** struct parsing functions and struct/macro
 
-;; The specification of struct/macro may change to facilitate
-;; the implementation of the downstream macros!
+;; *** TODO parse-field-spec
 
-;; Input:: (struct/macro id {field-spec | option | flag} ...)
-;; Output: (struct/macro/ { special-option }? id ( field-name ... ) ( field-spec ... } option-pair ...)
-;; id: symbol
-;; option: option-name option-value
-;; option-name: keyword ending in :
-;; option-value: symbol, keyword, boolean, tbd??
-;; flag: a keyword NOT ending in : as an abbreviation for an option
-;; - see (struct/macro-flag-to-option-values flag) above
-;; special-option: option-pair
-;; option-pair: ( option-name . option-value )
-;; field-name: symbol
-;; field-spec: list of ( contract field-option ...?  )
-;; contract: type | predicate function | racket contract
-;; field-option: tbd??
-(maybe-define-syntax (struct/macro stx)
-  ;; stx or anything ending in -stx is a syntax object
-  ;; stxs or anything ending in -stxs is a list of syntax objects
-  (let* ( [this 'struct/macro]
-          [args-stxs (cdr (syntax->list stx))]
-          [id-stx (car args-stxs)]
-          [spec-stxs (cdr args-stxs)] )
-    (printf "~a: ~a --> ~a\n" this 'id-stx id-stx)
-    (printf "~a: ~a --> ~a\n" this 'spec-stx spec-stxs)
-    ;; parse-field-spec takes a list of syntax objects specifying one field
-    ;; returns
-    ;; - a syntax object specifying the field name
-    ;; - a syntax object containing the rest of the field specification
-    (define (parse-field-spec stxs)
-      (define this 'parse-field-specs)
-      ;; stxs must be a non-empty list
-      (unless (and (pair? stxs) (list? stxs)) (raise-syntax-error #f "unknown struct specification" stx stxs))
-      (let ( [stx-1 (car stxs)]
-             [stxs-1 (cdr stxs)] )
-        (unless (identifier? stx-1) (raise-syntax-error #f "expected field name" stx stxs))
-        (unless (pair? stxs-1) (raise-syntax-error #f "expected field contract" stx stxs))
-        (values stx-1 (datum->syntax stx-1 stxs-1)) ) )
-    ;; parse takes
-    ;; - a list of syntax objects specifying a struct
-    ;; and returns four syntax objects, containing:
-    ;; - field-names (list of symbols)
-    ;; - field-specs (list beginning with contract followed by options)
-    ;; - special-option-pair/#f
-    ;; - option-values (list of pairs - the car of which is a colon-suffixed keyword)
-    ;; the lists within field-names and within field-specs correspond 1-1 and are non-empty
-    ;; - we're currently only checking that field-specs are non-empty!!
-    (define (parse-specs stxs [field-names-rev '()] [field-specs-rev '()] [special-option #f] [options-rev '()])
-      (define this 'parse-specs)
-      (printf "~a: ~a~a ~a~a ~a~a  ~a~a\n" this 'names field-names-rev 'specs field-specs-rev 'special special-option 'options options-rev)
-      ;; anything ending in -rev is a list of syntax objects in reverse order to their original appearance in stxs
-      (if (null? stxs)
-          ;; then return the four values, if valid
-          (begin
-            (unless (pair? field-specs-rev) (raise-syntax-error #f "expected field specification" stx))
-            (let* ( [field-names-stxs (reverse field-names-rev)] ; non-empty
-                    [field-specs-stxs (reverse field-specs-rev)] ; non-empty
-                    [options-stxs (reverse options-rev)] )
-              ;; return the lists as syntax objects using
-              ;; (datum->syntax context-syntax-object list-of-syntax-objects)
-              (values (datum->syntax (car field-names-stxs) field-names-stxs)
-                      (datum->syntax (car field-specs-stxs) field-specs-stxs)
-                      special-option
-                      (datum->syntax (if (pair? options-stxs) (car options-stxs) id-stx) options-stxs) ) ) )
-          ;; else continue parsing the specification
-          (let ( [stx-1 (car stxs)]       ; the 1st syntax object
-                 [stxs-1 (cdr stxs)] )    ; the rest of the spec (list of syntax objects)
-            (printf "~a: ~a --> ~a\n" this 'stx-1 stx-1)
-            (if (struct/macro-keyword? stx-1)
-                (let-values ( [(option special? rest) (struct/macro-option-values stx-1 stxs-1)] )
-                  (if special?
-                      (begin
-                        (when special-option (raise-syntax-error #f "only 1 special option permitted" special-option stx-1 stxs))
-                        (parse-specs rest field-names-rev field-specs-rev option options-rev) )
-                      (parse-specs rest field-names-rev field-specs-rev special-option (cons option options-rev)) ) )
-                ;; else, stx-1 "should" be a field specifier list - but is it?
-                (let-values ( [(name-stx spec-stx) (parse-field-spec (syntax-e stx-1))] )
-                  ;; continue to accumulate syntax
-                  (parse-specs stxs-1 (cons name-stx field-names-rev) (cons spec-stx field-specs-rev) special-option options-rev) ) ) ) ) )
-    (let-values ( [(names specs special-option options) (parse-specs spec-stxs)] )
-      (if special-option
-          #`(struct/macro/ #,special-option #,id-stx #,names #,specs #,options)
-          #`(struct/macro/ #,id-stx #,names #,specs #,options) ) ) ) )
+(maybe-begin-for-syntax
+
+ ;; TODO Replace this with real code!!
+ (define (struct/macro-expand-options stxs) stxs)
+ 
+ ;; parse-field-spec takes a list of syntax objects specifying one field
+ ;; (field-name contract {FIELD-OPTIONS}...?)
+ ;; returns
+ ;; - a syntax object specifying the field name
+ ;; - a syntax object containing the rest of the field specification
+ ;; Development feature you should not count on:
+;;  if the stxs are not wrapped as syntax objects we will return regular values!
+ (define (struct/macro-parse-field-spec stxs)
+   (unless (and (list? stxs)
+                (>= (length stxs) 2))
+     (raise-syntax-error #f "expected structure field specification" stxs))
+   (let* (
+          [field-name-stx (car stxs)]
+          [field-name (if (syntax? field-name-stx) (syntax-e field-name-stx) field-name-stx)]
+          [contract-stx (cadr stxs)]
+          [contract (if (syntax? contract-stx) (syntax-e contract-stx) contract-stx)]
+          [options-stx (struct/macro-expand-options (cddr stxs))] )
+     (unless (and (symbol? field-name) ; scheme
+                  #;(identifier field-name-stx) ; racket
+                  ;; contract isn't bound, so these won't work:
+                  #;(procedure? contract) ; scheme
+                  #;(contract? (cadr stxs)) ; racket -- requires a provide!
+                  )
+       (raise-syntax-error #f "invalid structure field specification" stxs) )
+     (values field-name-stx (struct/macro-stx field-name-stx (cons contract-stx options-stx))) ) )
+
+)
+ 
+;; *** TODO parse-specs
+
+(maybe-begin-for-syntax
+
+ ;; TODO need to pull out the code for expand-options
+ ;; and move it to the placeholder above!!
+ 
+ ;; parse-specs takes
+ ;; - start-stxs :: a list of syntax objects specifying a struct
+ ;;   - this is used for context in errors
+ ;; - stxs :: a list of syntax objects specifying a struct
+ ;;   - this starts out eq? to start-stxs
+ ;; and returns four syntax objects, containing:
+ ;; - field-names (list of symbols)
+ ;; - field-specs (list beginning with contract followed by options)
+ ;; - special-option-pair/#f
+ ;; - option-values (list of pairs - the car of which is a colon-suffixed keyword)
+ ;; the lists within field-names and within field-specs correspond 1-1 and are non-empty
+ ;; - we're currently only checking that field-specs are non-empty!!
+ (define (parse-specs start-stxs stxs [field-names-rev '()] [field-specs-rev '()] [special-option #f] [options-rev '()])
+   (define this 'parse-specs)
+   (show this)
+   (show field-names-rev)
+   (show field-specs-rev)
+   (show special-option)
+   (show options-rev)
+   ;; anything ending in -rev is a list of syntax objects in reverse order to their original appearance in stxs
+   (if (null? stxs)
+       ;; then return the four values, if valid
+       (begin
+         (unless (pair? field-specs-rev) (raise-syntax-error #f "expected field specification" start-stxs))
+         (let* ( [field-names-stxs (reverse field-names-rev)] ; non-empty
+                 [field-specs-stxs (reverse field-specs-rev)] ; non-empty
+                 [options-stxs (reverse options-rev)] )
+           ;; return the lists as syntax objects using
+           ;; (datum->syntax context-syntax-object list-of-syntax-objects)
+           (values (datum->syntax (car field-names-stxs) field-names-stxs)
+                   (datum->syntax (car field-specs-stxs) field-specs-stxs)
+                   special-option
+                   (datum->syntax (car (if (pair? options-stxs) options-stxs start-stxs)) ; syntax context
+                                  options-stxs ) ) ) )
+       ;; else continue parsing the specification
+       (let ( [stx-1 (car stxs)]       ; the 1st syntax object
+              [stxs-1 (cdr stxs)] )    ; the rest of the spec (list of syntax objects)
+         (printf "~a: ~a --> ~a\n" this 'stx-1 stx-1)
+         (if (struct/macro-keyword? stx-1)
+             (let-values ( [(option special? rest) (struct/macro-option-values stx-1 stxs-1)] )
+               (if special?
+                   (begin
+                     (when special-option (raise-syntax-error #f "only 1 special option permitted" special-option stx-1 stxs))
+                     (parse-specs stxs rest field-names-rev field-specs-rev option options-rev) )
+                   (parse-specs stxs rest field-names-rev field-specs-rev special-option (cons option options-rev)) ) )
+             ;; else, stx-1 "should" be a field specifier list - but is it?
+             (let-values ( [(name-stx spec-stx) (struct/macro-parse-field-spec (syntax-e stx-1))] )
+               ;; continue to accumulate syntax
+               (parse-specs stxs stxs-1 (cons name-stx field-names-rev) (cons spec-stx field-specs-rev) special-option options-rev) ) ) ) ) )
+ 
+ )
+
+(maybe-begin-for-syntax
+
+ ;; *** TODO struct/macro
+
+ ;; The specification of struct/macro may change to facilitate
+ ;; the implementation of the downstream macros!
+ 
+ ;; Input:: (struct/macro id {field-spec | option | flag} ...)
+ ;; Output: (struct/macro/ { special-option }? id ( field-name ... ) ( field-spec ... } option-pair ...)
+ ;; id: symbol
+ ;; option: option-name option-value
+ ;; option-name: keyword ending in :
+ ;; option-value: symbol, keyword, boolean, tbd??
+ ;; flag: a keyword NOT ending in : as an abbreviation for an option
+ ;; - see (struct/macro-flag-to-option-values flag) above
+ ;; special-option: option-pair
+ ;; option-pair: ( option-name . option-value )
+ ;; field-name: symbol
+ ;; field-spec: list of ( contract field-option ...?  )
+ ;; contract: type | predicate function | racket contract
+ ;; field-option: tbd??
+ (maybe-define-syntax (struct/macro stx)
+                      ;; stx or anything ending in -stx is a syntax object
+                      ;; stxs or anything ending in -stxs is a list of syntax objects
+                      (let* ( [this 'struct/macro]
+                              [args-stxs (cdr (syntax->list stx))]
+                              [id-stx (car args-stxs)]
+                              [spec-stxs (cdr args-stxs)] )
+                        (unless (identifier id-stx)
+                          (raise-syntax-error #f "expected structure id" stxs) )
+                        (show this)
+                        (show id-stx)
+                        (show spec-stxs)
+                        (let-values ( [(names specs special-option options) (parse-specs spec-stxs spec-stxs)] )
+                          (if special-option
+                              #`(struct/macro/ #,special-option #,id-stx #,names #,specs #,options)
+                              #`(struct/macro/ #,id-stx #,names #,specs #,options) ) ) ) )
+
+)
 
 ;; ** struct/macro/
 
